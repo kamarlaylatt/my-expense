44a6a12ce979bb2f782c58403193debff6b9bc90
kamarlaylatt <kamarehtha96@gmail.com>
2025-11-29 01:35:25 +0700

    fix: update import statement for PrismaClient for consistency

--- utils/prismaClient.js
+++ utils/prismaClient.js
@@ -1,6 +1,7 @@
 import pg from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
